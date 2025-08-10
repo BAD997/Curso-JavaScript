4ae517ad44941dfb4bea14b7742fce468464c3db
BAD997 <breinerabello2018@gmail.com>
2025-08-10 18:43:52 -0500

    Seccion 02 Palabras reservadas y nombre de variables

---
+++ Seccion-2/Palabras reservadas/assets/js/script.js
@@ -0,0 +1,30 @@
+/*
+  - Palabras reservadas:
+    Son palabras que tienen un uso específico para el cual fueron creadas.
+
+    Malas practicas:
+
+   let const = 123;
+
+   let 1abc = 123;
+
+   let await = 123;
+
+
+
+*/ 
+
+
+// Es buena practica que los nombre de las variables que asignemos tenga un sentido significativo.
+let objeto$ = 123;
+let precio99_99 = 123;
+
+// En vez de...
+let x = 'Fernando';
+
+// Usamos
+let nombreUsuario = 'Fernando';
+
+
+
+
