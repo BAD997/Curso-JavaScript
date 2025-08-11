4928c3384783968bee0f1f2658d147c0e0c180ff
BAD997 <breinerabello2018@gmail.com>
2025-08-11 16:36:55 -0500

    Seccion 02 Objetos literales

---
+++ Seccion-2/Objetos literales/assets/js/script.js
@@ -0,0 +1,10 @@
+/*
+ 
+    ¿Que son los objetos literales?
+
+    En JavaScript, un objeto literal es una forma concisa de definir un objeto directamente en el código utilizando la sintaxis de llaves {}. Este objeto se compone de pares clave-valor, donde cada clave (o propiedad) está asociada a un valor. Los objetos literales son una manera rápida y sencilla de crear objetos con datos específicos. 
+
+*/
+
+let 
+
