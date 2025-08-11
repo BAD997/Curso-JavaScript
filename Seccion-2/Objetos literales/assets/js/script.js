5f7688554c95ea1097166c4931dc729ca41febaf
BAD997 <breinerabello2018@gmail.com>
2025-08-11 17:02:20 -0500

    Seccion 02 Objetos literales

--- Seccion-2/Objetos literales/assets/js/script.js
+++ Seccion-2/Objetos literales/assets/js/script.js
@@ -6,5 +6,59 @@
 
 */
 
-let 
+let personaje = {
+
+    nombre: 'Tony Stark',
+    codeName: 'Ironman',
+    vivo: false,
+    edad:40,
+    coords: {
+        lat:34.034,
+        lng: -118.70
+    },
+    trajes: [
+        'Mark I',
+        'Mark V',
+        'Hulkbuster'
+    ],
+    direccion: {
+        zip: '1080, 90265',
+        ubicacion: 'Malibu, California'
+    },
+    'Ultima-Pelicula': 'Infinity War',
+};
+
+console.log(personaje);
+
+
+// Formas de  acceder a las propiedades del objeto
+
+console.log('Nombre: ',personaje.nombre);
+console.log('Code name: ',personaje['codeName']);
+console.log('Edad: ',personaje.edad);
+
+
+// Forma de acceder a las propiedades anidadas de un objeto
+
+console.log('Coors: ',personaje.coords);
+console.log('Lat: ',personaje.coords.lat);
+
+console.log('No. Trajes', personaje.trajes.length);
+
+console.log('Ultimo Traje', personaje.trajes[personaje.trajes.length - 1]);
+
+const x = 'vivo';
+
+console.log('Vivo:', personaje[x]);
+
+console.log('Ultima Pelicula: ',personaje["Ultima-Pelicula"] );
+
+
+
+
+
+
+
+
+
 
