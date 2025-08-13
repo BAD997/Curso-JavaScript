f108f242bffa699afec9cf5906999d4f082d3ff1
BAD997 <breinerabello2018@gmail.com>
2025-08-13 18:24:22 -0500

    Seccion 02 Funciones básicas y de flechas

---
+++ Seccion-2/Funciones básicas y de flecha/assets/js/script.js
@@ -0,0 +1,12 @@
+/*
+    ¿Que son las funciones en Javascript? 
+
+    En JavaScript, una función es un bloque de código reutilizable que realiza una tarea específica y puede ser llamado o invocado desde diferentes partes del código. Funciona como un procedimiento que toma entradas (parámetros) y puede devolver una salida. Las funciones son objetos de primera clase en JavaScript, lo que significa que pueden ser tratadas como cualquier otro objeto, asignadas a variables, pasadas como argumentos a otras funciones y devueltas como valores. 
+*/
+
+// Ejemplo de una funcion básica:
+
+function saludar(){
+    console.log('Hola Mundo');
+}
+
